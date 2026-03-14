09cb2f3cbdda5b7b0505e5dcb143deb02ffd2d84
wen <15180362811@163.com>
2026-03-14 22:40:44 +0800

    学习

---
+++ java_learning/LoopsExample.java
@@ -0,0 +1,14 @@
+// 示例：循环
+public class LoopsExample {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("for 循环: " + i);
+        }
+
+        int j = 0;
+        while (j < 5) {
+            System.out.println("while 循环: " + j);
+            j++;
+        }
+    }
+}
